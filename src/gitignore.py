b26f3950407b542bd1577076439ce4bf5be510ec
celsius narhwal <hello@celsiusnarhwal.dev>
2022-11-18 17:18:12 -0500

    add submodule for github/gitignore

--- src/gitignore.py
+++ src/gitignore.py
@@ -1,4 +1,4 @@
-import os
+import itertools
 import string
 from io import StringIO
 from pathlib import Path
@@ -12,48 +12,22 @@ import prompts
 
 inflect = ifl.engine()
 
+multiselect_controls = ("↑/↓: Move up and down\n"
+                        "Control + Z: Toggle selection\n"
+                        "Control + A: Toggle all on/off\n"
+                        "Enter: Confirm\n")
+
 
 class Underline:
     end = '\033[0m'
     underline = '\033[4m'
 
 
-class SelectKeybindings:
-    _universal = {
-        "skip": [{"key": "c-d"}],
-    }
-
-    def mono(self):
-        keybindings = {
-            "answer": [
-                {"key": "enter"},
-                {"key": "c-z"}
-            ],
-        }
-
-        return keybindings | self._universal
-
-    def multi(self):
-        keybindings = {
-            "toggle": [
-                {"key": "c-z"}
-            ],
-            "toggle-all-true": [
-                {"key": "c-a"}
-            ],
-            "toggle-all-false": [
-                {"key": "c-a"}
-            ],
-        }
-
-        return keybindings | self._universal
-
-
-def generate_gitignore(templates: List[str], remove_duplicates: bool, remove_comments: bool) -> str:
+def generate_gitignore(templates: List[Path], remove_duplicates: bool, remove_comments: bool) -> str:
     gitignore = StringIO()
 
     for template in templates:
-        for line in Path(f"templates/{template}.gitignore").open().readlines():
+        for line in template.open().readlines():
             if remove_duplicates and line in gitignore.getvalue() and line not in string.whitespace:
                 continue
 
@@ -62,7 +36,7 @@ def generate_gitignore(templates: List[str], remove_duplicates: bool, remove_com
 
             gitignore.write(line)
 
-        if template != templates[-1]:
+        if template != templates[-1] and not remove_comments:
             gitignore.write("\n")
 
     gitignore.seek(0)
@@ -70,7 +44,7 @@ def generate_gitignore(templates: List[str], remove_duplicates: bool, remove_com
     return gitignore.read()
 
 
-def save_gitignore(templates: List[str], remove_duplicates: bool, remove_comments: bool, use_cwd: bool = False) -> None:
+def save_gitignore(gitignore: str, use_cwd: bool = False) -> None:
     while True:
         if use_cwd:
             save_path = Path.cwd() / ".gitignore"
@@ -86,17 +60,14 @@ def save_gitignore(templates: List[str], remove_duplicates: bool, remove_comment
             save_path = prompts.filepath(
                 message="Where should your .gitignore be saved?",
                 instruction="Your answer will be interpreted as a directory.",
-                long_instruction="Both absolute and relative paths are cool. "
+                long_instruction="Both absolute and relative paths are supported."
                                  "Nonexistent directories will be created as necessary.",
-                mandatory_message="You can't skip this.",
                 multicolumn_complete=True,
                 only_directories=True,
                 validate=lambda path: bool(path),
                 invalid_message="You must enter a path.",
                 filter=path_postprocessor,
-                transformer=path_postprocessor,
-                qmark="•",
-                amark="✓").execute()
+                transformer=path_postprocessor).execute()
 
         if save_path.exists():
             overwrite_choices = [
@@ -106,13 +77,9 @@ def save_gitignore(templates: List[str], remove_duplicates: bool, remove_comment
 
             overwrite = prompts.select(
                 message="A .gitignore file already exists there. What do you wanna do?",
-                choices=overwrite_choices,
-                mandatory_message="You can't skip this.",
-                qmark="•",
-                amark="✓").execute()
+                choices=overwrite_choices).execute()
 
             if overwrite:
-                gitignore = generate_gitignore(templates, remove_duplicates, remove_comments)
                 save_path.write_text(gitignore)
                 break
             else:
@@ -123,34 +90,46 @@ def save_gitignore(templates: List[str], remove_duplicates: bool, remove_comment
             if save_path.name != ".gitignore":
                 save_path.mkdir(parents=True, exist_ok=True)
 
-            gitignore = generate_gitignore(templates, remove_duplicates, remove_comments)
-
             save_path.write_text(gitignore)
             break
 
     print(f"\n.gitignore saved to {save_path}.")
 
 
-def main():
-    multiselect_controls = ("↑/↓: Move up and down\n"
-                            "Control + Z: Toggle selection\n"
-                            "Control + A: Toggle all on/off\n"
-                            "Enter: Confirm\n")
+def get_templates() -> List[Path]:
+    categories = [
+        Choice(name="Languages and frameworks", value=["*.gitignore", "community/**/*.gitignore"]),
+        Choice(name="Editors and operating systems", value=["Global/*.gitignore"]),
+        Choice(name="Just show me everything", value=[]),
+    ]
 
-    template_names = [Path(f).with_suffix("").name for f in os.listdir("templates") if f.endswith(".gitignore")]
-    template_names.sort(key=str.casefold)
-    templates = prompts.fuzzy(
+    category = (prompts.select(message="Choose a category.", choices=categories).execute() or
+                itertools.chain.from_iterable([choices.value for choices in categories]))
+
+    repo_path = Path(__file__).parent / "gitignore"
+    templates: List[Path] = []
+
+    for template in itertools.chain(*[repo_path.glob(pattern) for pattern in category]):
+        name = template.stem
+
+        if template.parent.name == "Global" and any(t.name == template.stem for t in templates):
+            name += " (Global)"
+
+        templates.append(Choice(name=name, value=template))
+
+    templates.sort(key=lambda t: t.name.lower())
+
+    return prompts.fuzzy(
         message="Choose some templates.",
         instruction="You can type to search for specific ones.",
         long_instruction=multiselect_controls,
-        choices=template_names,
+        choices=templates,
         multiselect=True,
-        qmark="•",
-        amark="✓",
-        mandatory_message="You can't skip this.",
         transformer=lambda result: f"Generating from {inflect.no('template', len(result))}."
     ).execute()
 
+
+def get_modifiers() -> List[str]:
     modifier_choices = [
         Choice(name="Generate without duplicates", value="no_duplicates"),
         Choice(name="Generate without comments and empty lines", value="no_comments"),
@@ -165,18 +144,18 @@ def main():
         words = [x for sublist in [modifier_transformations[r] for r in result] for x in sublist]
         return f"Generating without {inflect.join(words, conj='or')}."
 
-    modifiers = prompts.select(message="Cool. Now choose some options.",
+    modifiers = prompts.select(message="Choose some options.",
                                instruction="(You can also skip this question with Control + D.)",
                                long_instruction=multiselect_controls,
                                choices=modifier_choices,
                                multiselect=True,
                                mandatory=False,
-                               qmark="•",
-                               amark="✓",
                                transformer=lambda result: modifier_transformer(result)).execute() or []
 
-    remove_duplicates, remove_comments = "no_duplicates" in modifiers, "no_comments" in modifiers
+    return modifiers
 
+
+def get_output() -> str:
     output_choices = [
         Choice(name="Save to current directory", value="cwd"),
         Choice(name="Save to another directory", value="elsewhere"),
@@ -184,21 +163,29 @@ def main():
         Choice(name="Copy to clipboard", value="clipboard"),
     ]
 
-    output = prompts.select(message="Gotcha. Now choose a place to save your .gitignore.",
-                            choices=output_choices,
-                            qmark="•",
-                            amark="✓",
-                            mandatory_message="You can't skip this.").execute()
+    output = prompts.select(message="Choose how to save your .gitignore.",
+                            choices=output_choices).execute()
+
+    return output
+
+
+def main():
+    print("tuneout © 2022-present celsius narhwal. Licensed under MIT (see --license).\n")
+    templates = get_templates()
+    modifiers = get_modifiers()
+    output = get_output()
+
+    remove_duplicates, remove_comments = "no_duplicates" in modifiers, "no_comments" in modifiers
+
+    gitignore = generate_gitignore(templates, remove_duplicates, remove_comments)
 
     if output == "cwd":
-        save_gitignore(templates, remove_duplicates, remove_comments, use_cwd=True)
+        save_gitignore(gitignore, use_cwd=True)
     elif output == "elsewhere":
-        save_gitignore(templates, remove_duplicates, remove_comments)
+        save_gitignore(gitignore)
     elif output == "stdout":
-        gitignore = generate_gitignore(templates, remove_duplicates, remove_comments)
         print(f"\n{Underline.underline}Generated .gitignore{Underline.end}\n{gitignore}")
     elif output == "clipboard":
-        gitignore = generate_gitignore(templates, remove_duplicates, remove_comments)
         pyperclip.copy(gitignore)
         print("\n.gitignore copied to clipboard.")
 
--- src/prompts.py
+++ src/prompts.py
@@ -1,7 +1,7 @@
 from InquirerPy import inquirer
 
 
-def keybindings_decorator(func):
+def prompt_decorator(func):
     def wrapper(*args, **kwargs):
         if kwargs.get("multiselect"):
             keybindings = {
@@ -26,57 +26,60 @@ def keybindings_decorator(func):
         keybindings["skip"] = [{"key": "c-d"}]
 
         kwargs["keybindings"] = keybindings
+        kwargs["qmark"] = "•"
+        kwargs["amark"] = "✓"
+        kwargs["mandatory_message"] = "You can't skip this."
 
         return func(*args, **kwargs)
 
     return wrapper
 
 
-@keybindings_decorator
+@prompt_decorator
 def checkbox(*args, **kwargs):
     return inquirer.checkbox(*args, **kwargs)
 
 
-@keybindings_decorator
+@prompt_decorator
 def confirm(*args, **kwargs):
     return inquirer.confirm(*args, **kwargs)
 
 
-@keybindings_decorator
+@prompt_decorator
 def expand(*args, **kwargs):
     return inquirer.expand(*args, **kwargs)
 
 
-@keybindings_decorator
+@prompt_decorator
 def filepath(*args, **kwargs):
     return inquirer.filepath(*args, **kwargs)
 
 
-@keybindings_decorator
+@prompt_decorator
 def fuzzy(*args, **kwargs):
     return inquirer.fuzzy(*args, **kwargs)
 
 
-@keybindings_decorator
+@prompt_decorator
 def text(*args, **kwargs):
     return inquirer.text(*args, **kwargs)
 
 
-@keybindings_decorator
+@prompt_decorator
 def select(*args, **kwargs):
     return inquirer.select(*args, **kwargs)
 
 
-@keybindings_decorator
+@prompt_decorator
 def number(*args, **kwargs):
     return inquirer.number(*args, **kwargs)
 
 
-@keybindings_decorator
+@prompt_decorator
 def rawlist(*args, **kwargs):
     return inquirer.rawlist(*args, **kwargs)
 
 
-@keybindings_decorator
+@prompt_decorator
 def secret(*args, **kwargs):
     return inquirer.secret(*args, **kwargs)
